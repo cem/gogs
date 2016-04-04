434388b34187e2b8074305fca352c2551e1ccc1d
Cem Kocagil <cem@users.noreply.github.com>
2016-04-04 04:13:12 +0300

    Fix assets after merge

--- routers/repo/http.go
+++ routers/repo/http.go
@@ -22,7 +22,6 @@ import (
 	"time"
 
 	"github.com/gogits/gogs/models"
-	"github.com/gogits/gogs/modules/base"
 	"github.com/gogits/gogs/modules/context"
 	"github.com/gogits/gogs/modules/log"
 	"github.com/gogits/gogs/modules/setting"
